b530b94db126b207b032e232417b7a3edb8b2490
tiffany <tiffanylee32@gmail.com>
2018-09-22 22:55:01 -0700

    [Update] open line extension: add android messages

---
+++ Code/openLineExtension/logIntoLine.js
@@ -0,0 +1,10 @@
+(function() {
+    'use strict';
+
+    // TODO: find the dom:wq
+    // put pw into => id = line_login_pwd
+    // click => id = login_btn
+    $('#line_login_pwd').val('thecryingtree');
+    $('#login_btn').click();
+
+})();
--- Code/openLineExtension/openLineExtension.js
+++ Code/openLineExtension/openLineExtension.js
@@ -3,4 +3,10 @@ chrome.runtime.onStartup.addListener(function() {
         url: 'chrome-extension://ophjlpahpchlmihnnnihgmmeilfjmjjc/index.html',
         type: 'popup'
     });
+    chrome.windows.create({
+        url: 'https://messages.android.com',
+        type: 'popup'
+    });
+
+
 });
